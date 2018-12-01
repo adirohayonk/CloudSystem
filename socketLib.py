eb5c4dcf9aaf8c43c948ed3660e421dcddc5dc52
Adir Ohayon <adirohayonk@gmail.com>
2018-12-01 16:01:09 +0200

    removed receive_file old function create new function

--- socketLib.py
+++ socketLib.py
@@ -1,4 +1,8 @@
-def send_and_encode(data, sock):
+import fileManagementLib as fileM
+import datetime
+import os
+
+def send_and_encode(sock, data):
     encodedData = data.encode()
     sock.send(encodedData)
 
@@ -8,27 +12,38 @@ def recv_and_decode(sock):
     decodedData = data.decode()
     decodedData = decodedData.strip()
     return decodedData
+    
+
+def receiveFile(sock, filename):
+    fileSize = recv_and_decode(sock)
+    send_and_encode(sock, "File size is: " + fileSize + " Bytes "
+     + "Tranferring file...")
+    currentTime = datetime.datetime.now().strftime("%d-%m-%Y-%X") 
+    storedFileName = "[" + filename + "]" + currentTime
+    print("Saving file filename is {}".format(storedFileName))
+    f = open("clients_jobs/" + storedFileName, 'wb')
+    data = sock.recv(2048)
+    totalRecv = len(data) 
+    f.write(data)
+    while totalRecv < int(fileSize):
+        data = sock.recv(2048)
+        f.write(data)
+        totalRecv += len(data) 
+    print("File Transfer completed")
 
 
-def receive_file(sock, filename):
-    if filename != 'q':
-        send_and_encode(filename, sock)
-        data = recv_and_decode(sock)
-        if data[:6] == 'EXISTS':
-            filesize = data[6:]
-            message = input("File Exists, " + str(filesize)+"Bytes, download? (Y/N)? ->")
-            if message == 'y':
-                send_and_encode("OK", sock)
-                f = open('new_'+filename, 'wb')
-                data = sock.recv(1024)
-                totalRecv = len(data)
-                f.write(data)
-                while totalRecv < int(filesize):
-                    data = sock.recv(1024)
-                    totalRecv += len(data.decode())
-                    f.write(data)
-                    print("{0:.2f}".format((totalRecv/float(filesize))*100)+" %Done")
-                print("Download Complete!")
-        else:
-            print("File does not Exist!")
+def sendFile(sock, filename):
+    if os.path.isfile(filename):
+        fileSize = os.path.getsize(filename)
+        send_and_encode(sock, str(fileSize))
+        response = recv_and_decode(sock)
+        print(response)
+        with open(filename, 'rb') as f:
+                bytesToSend = f.read(1024)
+                sock.send(bytesToSend)
+                while bytesToSend != "":
+                    bytesToSend = f.read(1024)
+                    sock.send(bytesToSend)
+    else:
+        print("Filename doesn't exists")
     sock.close()
