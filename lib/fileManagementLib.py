982afae9d1392ce75044f3d411cc812b3d5091ee
Adir Ohayon <adirohayonk@gmail.com>
2018-12-02 12:48:12 +0200

    add setenv function

--- lib/fileManagementLib.py
+++ lib/fileManagementLib.py
@@ -1,7 +1,17 @@
 import os
+import pathlib
 
-def checkFiles(filename):
+def openFile(folder ,filename):
+    if not os.path.exists(folder):
+        os.makedirs(folder)
     if os.path.isfile(filename):
         return True
     else: 
         return False
+
+def createEnv():
+    jobsFolder = 'jobs/'
+    libFolder = '../lib/'
+    pathlib.Path(jobsFolder).mkdir(parents=True, exist_ok=True)
+    if not os.path.exists(libFolder):
+        raise Exception('lib folder is missing in {}'.format(os.path.abspath(libFolder)))
